9432fddedc283fdb013a1170fffdffd2ead1c16b
m4nuela <manu_ela10@hotmail.com>
2016-09-25 12:26:20 -0300

    sem levar em consideracao multilabel

--- Main.py
+++ Main.py
@@ -6,32 +6,46 @@ from nltk.corpus import stopwords
 import string
 import re
 
-categories = ['earn','acq','money-fx', 'grain','crude', 'trade', 'interest','ship','wheat', 'corn']
+import random
+
+categories = ['earn', 'acq', 'money-fx', 'grain', 'crude', 'trade', 'interest', 'ship', 'wheat', 'corn']
 
 porterStemmer = PorterStemmer()
 stopSet = set(stopwords.words('english'))
 punctuations = set(string.punctuation)
 stopAndPuctuationSet = stopSet | punctuations
 
-print(stopAndPuctuationSet)
+
+# print(stopAndPuctuationSet)
 
 def checkNotANumber(word):
-    obj = re.match(r'[a-z][a-z]+',word,re.I)
+    obj = re.match(r'[a-z][a-z]+', word, re.I)
     if obj:
         if obj.group() == word:
             return True
     return False
 
+
 def findCategory(clist):
-    c = [0]*len(categories)
+    c = [0] * len(categories)
     for i in range(0, len(c)):
         if categories[i] in clist:
             c[i] += 1
     return c
 
+
 def preprocessFile(file):
     tokens = nltk.word_tokenize(' '.join(reuters.words(file)))
-    category = findCategory(reuters.categories(file))
+
+    # # we should be using this one
+    #category = findCategory(reuters.categories(file))
+
+
+    # # but for now we can test with this
+    category = [x for x in reuters.categories(file) if x in categories][0]
+
+
+
     document = Document(file, category)
     content = []
     for token in tokens:
@@ -42,24 +56,28 @@ def preprocessFile(file):
     document.setContent(content)
     return document
 
-#print(reuters.fileids())
-#print(reuters.categories())
+
+def makeDictionary(array):
+    return dict([[x, "True"] for x in array])
+
+
+# print(reuters.fileids())
+# print(reuters.categories())
 
 
 files = reuters.fileids(categories)
+# files = files[:100]
 
+# ACQ 2369 != 1829
+# TRADE 488 != 485
+# CORN  237 != 238
 
-#ACQ 2369 != 1829
-#TRADE 488 != 485
-#CORN  237 != 238
-
-#print(len(reuters.fileids('acq')))
+# print(len(reuters.fileids('acq')))
 
 trainingSet = []
 testSet = []
 
 for file in files:
-    #print(reuters.categories(file));
     preprocessedFile = preprocessFile(file)
 
     index = file.find('training')
@@ -70,13 +88,19 @@ for file in files:
         if (index != -1):
             testSet.append(preprocessedFile)
 
-
 print('----Training Set----')
 print(len(trainingSet))
-print(trainingSet)
-
+# print(trainingSet)
 
 print('\n')
 print('----Test Set----')
 print(len(testSet))
-print(testSet)
+# print(testSet)
+
+
+training = [[makeDictionary(x.getContent()), x.getCategory()] for x in trainingSet]
+testing = [[makeDictionary(x.getContent()), x.getCategory()] for x in testSet]
+
+## naive bayes
+classifier = nltk.NaiveBayesClassifier.train(training)
+print nltk.classify.accuracy(classifier, testing)
